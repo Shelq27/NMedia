46c6cf1b6465f089fc18f201ef1be80a46fef4b5
Shelq <klcao@yandex.ru>
2023-06-01 08:57:59 +0300

    Dz MVVM

---
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
@@ -0,0 +1,12 @@
+package ru.netology.nmedia.repository
+
+import androidx.lifecycle.LiveData
+import ru.netology.nmedia.dto.Post
+
+interface PostRepository {
+
+    fun get(): LiveData<Post>
+    fun like()
+    fun  repost()
+
+}
