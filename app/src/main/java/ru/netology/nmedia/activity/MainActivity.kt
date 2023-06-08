2e13a89a0b8334ae3b485d6a581df5d41e035189
Shelq <klcao@yandex.ru>
2023-06-08 15:36:19 +0300

    Added DZ RecyclerView

--- app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -4,15 +4,9 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import ru.netology.nmedia.R
+import ru.netology.nmedia.adapter.PostsAdapter
 import ru.netology.nmedia.databinding.ActivityMainBinding
-import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.viewmodel.PostViewModel
-import java.math.RoundingMode
-import java.text.DecimalFormat
-import kotlin.math.floor
-import kotlin.math.log10
-
 
 
 @SuppressLint("StaticFieldLeak")
@@ -28,43 +22,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val viewModel by viewModels<PostViewModel>()
-        viewModel.data.observe(this){post->
-            with(binding) {
-                AuthorTv.text = post.author
-                PublishedTv.text = post.published
-                ContentTv.text = post.content
-                NumberOfLikesTv.text = prettyCount(post.likes)
-                NumberOfRepostsTv.text = prettyCount(post.reposted)
-                statusLike(post, binding)
-            }
-        }
-        binding.LikeIb.setOnClickListener {
-            viewModel.like()
-        }
-        binding.RepostIb.setOnClickListener {
-            viewModel.repost()
-        }
+        val adapter = PostsAdapter({
+            viewModel.likeById(it.id)
+        }, {
+            viewModel.repostById(it.id)
+        })
+        binding.list.adapter = adapter
+        viewModel.data.observe(this) { posts ->
+            adapter.submitList(posts)
 
+        }
     }
+
+
 }
 
-fun statusLike(post: Post, binding: ActivityMainBinding): Boolean {
-    return if (post.likedByMe) {
-        binding.LikeIb.setImageResource(R.drawable.ic_liked)
-        true
-    } else {
-        binding.LikeIb.setImageResource(R.drawable.ic_like)
-        false
-    }
-}
-fun prettyCount(numb : Int): String? {
-    val value = floor(log10(numb.toDouble())).toInt()
-    return when {
-        value < 3 -> numb.toString() // < 1000
-        value < 4 -> DecimalFormat("#.#").apply { roundingMode=RoundingMode.FLOOR }.format(numb.toDouble() / 1000) + "K" // < 10_000
-        value < 6 -> DecimalFormat("#").apply { roundingMode=RoundingMode.FLOOR }.format(numb.toDouble() / 1000) + "K" // < 1_000_000
-        else -> DecimalFormat("#.#").apply { roundingMode=RoundingMode.FLOOR }.format(numb.toDouble() / 1_000_000) + "M"
-    }
-}
 
 
--- app/src/main/java/ru/netology/nmedia/dto/Post.kt
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -5,7 +5,7 @@ data class Post(
     val author: String,
     val content: String,
     val published: String,
-    val likes: Int =1999,
+    val likes: Int =0,
     val reposted : Int = 0,
     val likedByMe: Boolean = false
 )
