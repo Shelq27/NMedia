c4bf1023c433acb024e9b1b143b80b93f4770b8e
Shelq <klcao@yandex.ru>
2023-10-12 21:41:44 +0300

    DzPush

--- app/src/main/java/ru/netology/nmedia/dto/Post.kt
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -2,7 +2,7 @@ package ru.netology.nmedia.dto
 
 data class Post(
     val id: Long,
-    val author: String,
+    val author: String ,
     val content: String,
     val published: String,
     val likes: Int = 0,
