8b2ed4901af8695566795716db8ca686340de3d4
Shelq <klcao@yandex.ru>
2024-03-05 16:31:00 +0300

    edited DbModule

--- app/src/main/java/ru/netology/nmedia/db/AppDb.kt
+++ app/src/main/java/ru/netology/nmedia/db/AppDb.kt
@@ -1,33 +1,14 @@
 package ru.netology.nmedia.db
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import ru.netology.nmedia.dao.PostDao
 import ru.netology.nmedia.dao.PostRemoteKeyDao
 import ru.netology.nmedia.entity.PostEntity
 import ru.netology.nmedia.entity.PostRemoteKeyEntity
 
-@Database(entities = [PostEntity::class, PostRemoteKeyEntity::class], version = 2)
+@Database(entities = [PostEntity::class, PostRemoteKeyEntity::class], version = 2, exportSchema = false)
 abstract class AppDb : RoomDatabase() {
     abstract fun postDao(): PostDao
     abstract fun postRemoteKeyDao(): PostRemoteKeyDao
-
-    companion object {
-
-        @Volatile
-        private var instance: AppDb? = null
-
-        fun getInstance(context: Context): AppDb {
-            return instance ?: synchronized(this) {
-                instance ?: buildDatabase(context).also { instance = it }
-            }
-        }
-
-        private fun buildDatabase(context: Context) =
-            Room.databaseBuilder(context, AppDb::class.java, "app.db")
-                .fallbackToDestructiveMigration()
-                .build()
-    }
 }
